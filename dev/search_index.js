0190ee423fff12cba8690a5f704328aa312b4ef1
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-26 15:08:52 +0000

    build based on 667e72a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"-","title":"-","text":"CurrentModule = AComVarDesign","category":"page"},{"location":"#","page":"-","title":"-","text":"Design","category":"page"},{"location":"#AComVarDesign.Design","page":"-","title":"AComVarDesign.Design","text":"Constructor\n\n\n\n\n\n","category":"type"}]
+}
