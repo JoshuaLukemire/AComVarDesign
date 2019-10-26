3a2ba76fba564ac0d24987175d8b3bd2bf485ffd
Joshua Lukemire <joshlukemire@gmail.com>
2019-10-26 10:40:53 -0400

    trying to get docs setup with travis

--- docs/make.jl
+++ docs/make.jl
@@ -2,5 +2,9 @@ push!(LOAD_PATH,"../src/")
 push!(LOAD_PATH,"../../")
 
 using Documenter, AComVarDesign
- 
+
 makedocs(sitename="My Documentation")
+
+deploydocs(
+    repo = "github.com/JoshuaLukemire/AComVarDesign.git",
+)
